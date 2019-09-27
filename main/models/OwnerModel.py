b549abdd92dce5859350a110d89f2533c05a2ce0
Han Xu <hanx-2011@hotmail.com>
2019-09-26 23:12:15 -0700

    Added database queries to owner model and rendered table to view

--- main/models/OwnerModel.py
+++ main/models/OwnerModel.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from main import db
 
 
@@ -10,10 +8,38 @@ class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_name = db.Column(db.String(20), nullable=False)
     owner_email = db.Column(db.String(50), unique=True, nullable=False)
-    joined_at = db.Column(db.DateTime, nullable=False, default=date.today)
+    joined_at = db.Column(db.DateTime, nullable=False)
 
     # each owner can relate to many contents
     contents = db.relationship('Content', backref='owners', lazy=True)
 
+    def __init__(self, owner_name, owner_email, joined_at):
+        self.owner_name = owner_name
+        self.owner_email = owner_email
+        self.joined_at = joined_at
+
     def __repr__(self):
         return f"Owner('{self.owner_name}','{self.owner_email}')"
+
+    @classmethod
+    def find_by_email(cls, email):
+        # query rows with the correct email, and fetch the first record
+        return cls.query.filter_by(owner_email=email).first()
+
+    @classmethod
+    def find_by_name(cls, name):
+        # query rows with the correct owner name, and fetch all records
+        return cls.query.filter_by(owner_name=name).all()
+
+    @classmethod
+    def get_all_owners(cls):
+        return cls.query.all()
+
+    def save_owner(self):
+        # add() creates a new record if not exist, otherwise updates existing
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_owner(self):
+        db.session.delete(self)
+        db.session.commit()
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, redirect, url_for
+from datetime import date
 
 from main import app
 from main.models.ContentModel import Content
@@ -24,24 +25,38 @@ def content():
         # build dictionary from all choices. then get value from choice data
         choice = form.content_type.data
         choices = dict(ContentForm.SELECT_CHOICES)
-
         flash(f'Your {(choices.get(choice)).lower()} has been added!')
         return redirect(url_for('inventory'))
 
     return render_template('content_form.html', title='Content Form', form=form)
 
-@app.route("/owner", methods=['GET', 'POST'])
+@app.route("/owners", methods=['GET', 'POST'])
 def owner():
     # Owner page is reset after user submits OwnerForm
     form = OwnerForm()
 
+    # Following if block executes when the form is submitted and validated
     if form.validate_on_submit():
+        # obtain the data from the form and clear the form fields
         name = form.owner_name.data
         email = form.owner_email.data
-
         form.owner_email.data = ''
         form.owner_name.data = ''
 
-        flash(f'User {name} has been added!')
+        # validate whether owner already exists
+        if Owner.find_by_email(email):
+            flash(f'User with email {email} already exists!')
 
-    return render_template('owner_form.html', title='Owner Form', form=form)
+        # create and save the new owner with form data
+        new_owner = Owner(owner_name=name,
+                          owner_email=email,
+                          joined_at=date.today().split(' ')[0])
+        new_owner.save_owner()
+        flash(f'User {name} has been created!')
+
+    # Get all existing owners from the database and render the view
+    owners = Owner.get_all_owners()
+    return render_template('owner_form.html',
+                           title='Owner Form',
+                           form=form,
+                           owners=owners)
